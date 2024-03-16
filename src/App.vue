f221c6b29e4baaa77764599c4e6084ae6b8c7760
JuampiTurner <juampiturnerlopez@gmail.com>
2024-03-16 12:42:18 -0300

    added styles for layout (and responsive)

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
-<script setup>
-
-</script>
-
 <template>
-  <RouterView />
+  <div class="layout">
+    <header class="alatsi-regular">Header</header>
+    <aside class="sidebar" @click="toggleSidebar">Tasks/GroupsList</aside>
+    <main>TaskInfo</main>
+    <aside class="sidebar2">TaskConfigurations</aside>
+    <footer>Footer</footer>
+  </div>
 </template>
 
+<style scoped>
+@import './assets/style.css';
+@import url('https://fonts.googleapis.com/css2?family=Alatsi&display=swap');
+</style>
